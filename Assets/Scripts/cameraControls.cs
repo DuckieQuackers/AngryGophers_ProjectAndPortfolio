8e0d9efa23c09bdc9f494bc823c79831beaf0504
TDHall2 <tdhall2@student.fullsail.edu>
2022-10-04 15:02:14 -0400

    Added camera controls, player movement, and sprint

---
+++ Assets/Scripts/cameraControls.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControls : MonoBehaviour
+{
+    [SerializeField] float horSens;
+    [SerializeField] float vertSens;
+    [SerializeField] float lookVertMax;
+    [SerializeField] float lookVertMin;
+    [SerializeField] bool invert;
+    float xRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lock cursor to middle and make invisible
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //camera input
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * horSens;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * vertSens;
+        if (invert)
+        {
+            xRotation -= mouseY;
+        }
+        else
+        {
+            xRotation += mouseY;
+        }
+
+        //clamp xRotation
+        xRotation = Mathf.Clamp(xRotation, lookVertMin, lookVertMax);
+        //rotate camera
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        //rotate player
+        transform.parent.Rotate(Vector3.up * mouseX);
+
+    }
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] float sprintSpeed;
+    [SerializeField] CharacterController controller;
+    [SerializeField] float playerSpeed;
+    [SerializeField] float jumpHeight;
+    [SerializeField] float gravityModifier;
+    private Vector3 playerVelocity;
+    [SerializeField] int jumpsMax;
+    private int jumpCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //resetting playerVelocity and jumpCount
+        if (controller.isGrounded && playerVelocity.y < 0)
+        {
+            jumpCount = 0;
+            playerVelocity.y = 0;
+        }
+        //Player Movement and Sprint
+        Vector3 move = (transform.right * Input.GetAxis("Horizontal") + (transform.forward * Input.GetAxis("Vertical")));
+        if (Input.GetButton("Sprint"))
+        {
+            controller.Move(move * Time.deltaTime * sprintSpeed);
+        }
+        else
+        {
+            controller.Move(move * Time.deltaTime * playerSpeed);
+        }
+
+        //jumping
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpsMax)
+        {
+            jumpCount++;
+            playerVelocity.y = jumpHeight;
+        }
+        playerVelocity.y -= gravityModifier * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+
+    }
+}
