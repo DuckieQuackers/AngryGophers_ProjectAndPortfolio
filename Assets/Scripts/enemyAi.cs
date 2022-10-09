096873c72d1eded3c0443e08d6822d2ffc7802dc
JakeWilson130 <jakewilson130@gmail.com>
2022-10-09 15:12:12 -0400

    merged with main

--- Assets/Scripts/enemyAi.cs
+++ Assets/Scripts/enemyAi.cs
@@ -3,63 +3,93 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class enemyAi : MonoBehaviour, iDamage
+public class enemyAI : MonoBehaviour, IDamage
 {
-    [Header ("----- Components -----")]
+    [Header("----- Components -----")]
     [SerializeField] NavMeshAgent agent;
-    [SerializeField] GameObject bullet;
-    [SerializeField] GameObject bulletOrigin;
+    [SerializeField] Renderer model;
 
-    [Header("----- Enemy stats -----")]
-    [SerializeField] int hp;
+    [Header("----- Enemy Stats -----")]
+    [SerializeField] int HP;
+    [SerializeField] int facePlayerSpeed;
+    [SerializeField] int sightDist;
+
+    [Header("----- Gun Stats -----")]
     [SerializeField] float shootRate;
+    [SerializeField] GameObject bullet;
+    [SerializeField] GameObject shootPos;
 
-
-    bool playerInRange;
     bool isShooting;
+    public bool playerInRange;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager.instance.enemyNumber++;
+        gameManager.instance.enemyCountText.text = gameManager.instance.enemyNumber.ToString("F0");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerInRange)
+        if (agent.enabled && playerInRange)
         {
-            agent.SetDestination(GameObject.FindGameObjectWithTag("Player").transform.position);
 
-            if(!isShooting)
+            agent.SetDestination(gameManager.instance.player.transform.position);
+
+            if (!isShooting)
+            {
                 StartCoroutine(shoot());
+            }
         }
     }
 
     public void takeDamage(int dmg)
     {
-        hp -= dmg;
-        
-        if(hp <= 0)
+        HP -= dmg;
+
+        StartCoroutine(flashDamage());
+        if (HP <= 0)
+        {
+            gameManager.instance.checkEnemyTotal();
             Destroy(gameObject);
+        }
+
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-            playerInRange = true;
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.CompareTag("Player"))
-            playerInRange= false;
-    }
     IEnumerator shoot()
     {
         isShooting = true;
-        Instantiate(bullet, bulletOrigin.transform.position, transform.rotation);
+
+        Instantiate(bullet, shootPos.transform.position, transform.rotation);
+
+
         yield return new WaitForSeconds(shootRate);
         isShooting = false;
     }
+
+    IEnumerator flashDamage()
+    {
+        model.material.color = Color.red;
+        agent.enabled = false;
+        yield return new WaitForSeconds(0.25f);
+        model.material.color = Color.white;
+        agent.enabled = true;
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
 }
--- Assets/Scripts/iDamage.cs
+++ Assets/Scripts/iDamage.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface iDamage
+public interface IDamage
 {
-    void takeDamage(int dmg);
+     void takeDamage(int dmg);
 }
