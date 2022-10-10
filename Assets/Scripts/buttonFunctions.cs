d635f5fd41b2d62460ba4505bf7ff30a4c4acb18
JakeWilson130 <jakewilson130@gmail.com>
2022-10-09 21:59:35 -0400

    added game manager script

---
+++ Assets/Scripts/buttonFunctions.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonFunctions : MonoBehaviour
+{
+   
+    public void resume()
+    {
+        gameManager.instance.cursorUnlockUnpause();
+        gameManager.instance.pauseMenu.SetActive(false);
+        gameManager.instance.isPaused = false;
+    }
+    public void restart()
+    {
+        gameManager.instance.cursorUnlockUnpause();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void respawn()
+    {
+        gameManager.instance.playerScript.respawn();
+        gameManager.instance.cursorUnlockUnpause();
+
+    }
+}
